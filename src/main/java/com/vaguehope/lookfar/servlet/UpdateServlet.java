3b794597c0e5f4561ad292c433139927fa1304a4
はく <haku@vaguehope.com>
2012-08-12 21:53:14 +0100

    fix date format

--- src/main/java/com/vaguehope/lookfar/servlet/UpdateServlet.java
+++ src/main/java/com/vaguehope/lookfar/servlet/UpdateServlet.java
@@ -41,7 +41,7 @@ public class UpdateServlet extends HttpServlet {
 
 	@Override
 	protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Table<Integer, String, String> table = TreeBasedTable.create();
 		try {
 			int i = 0;
