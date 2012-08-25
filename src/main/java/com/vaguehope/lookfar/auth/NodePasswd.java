2b2b2eb75b043683ab0428d2c2980537a959b387
はく <haku@vaguehope.com>
2012-08-25 10:56:57 +0100

    Simple passwd hash cache.

--- src/main/java/com/vaguehope/lookfar/auth/NodePasswd.java
+++ src/main/java/com/vaguehope/lookfar/auth/NodePasswd.java
@@ -2,20 +2,29 @@ package com.vaguehope.lookfar.auth;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.mindrot.jbcrypt.BCrypt;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.vaguehope.lookfar.model.DataStore;
 import com.vaguehope.lookfar.servlet.ServletHelper;
 
 public class NodePasswd implements PasswdChecker {
 
-	private final DataStore dataStore;
+	private final LoadingCache<String, String> paswdCache;
 
 	public NodePasswd (DataStore dataStore) {
-		this.dataStore = dataStore;
+		// FIXME Move cache to DataStore so entries expire when modified.
+		this.paswdCache = CacheBuilder.newBuilder()
+				.expireAfterWrite(10, TimeUnit.MINUTES)
+				.maximumSize(500)
+				.build(new PasswdLoader(dataStore));
 	}
 
 	@Override
@@ -23,13 +32,28 @@ public class NodePasswd implements PasswdChecker {
 		try {
 			String nodeName = ServletHelper.extractPathElement(req);
 			if (nodeName == null || !nodeName.equals(user)) return false;
-			String hashpw = this.dataStore.getNodeHashpw(nodeName); // TODO Guava Cache this.
+			String hashpw = this.paswdCache.get(nodeName);
 			if (hashpw == null) return false;
 			return BCrypt.checkpw(pass, hashpw);
 		}
-		catch (SQLException e) {
+		catch (ExecutionException e) {
 			throw new IOException(e.getMessage(), e);
 		}
+	}
+
+	private static class PasswdLoader extends CacheLoader<String, String> {
+
+		private final DataStore dataStore;
+
+		public PasswdLoader (DataStore dataStore) {
+			this.dataStore = dataStore;
+		}
+
+		@Override
+		public String load (String nodeName) throws SQLException {
+			return this.dataStore.getNodeHashpw(nodeName);
+		}
+
 	}
 
 }
