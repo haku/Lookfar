33b7a468d23917f2dcfe538a095d21f0fd091088
はく <haku@vaguehope.com>
2012-11-18 12:28:35 +0000

    Send all Splunk updates via queue.

--- src/main/java/com/vaguehope/lookfar/splunk/SplunkProducer.java
+++ src/main/java/com/vaguehope/lookfar/splunk/SplunkProducer.java
@@ -3,9 +3,14 @@ package com.vaguehope.lookfar.splunk;
 import java.io.IOException;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.rabbitmq.client.Channel;
 
 public class SplunkProducer {
+
+	private static final Logger LOG = LoggerFactory.getLogger(SplunkProducer.class);
 
 	private final Splunk splunk;
 	private final Channel chan;
@@ -20,7 +25,13 @@ public class SplunkProducer {
 	public void scheduleUpdate (String node, Map<String, String> data) {
 		if (this.splunk.isEnabled()) {
 			String body = Splunk.splunkDataString(node, data, System.currentTimeMillis());
-			this.splunk.getExcutor().submit(new SplunkDirectPut(body, this.splunk, this));
+			//this.splunk.getExcutor().submit(new SplunkDirectPut(body, this.splunk, this));
+			try {
+				rescheduleUpdate(body);
+			}
+			catch (IOException e) {
+				LOG.warn("Failed to reschedule update: {}", e.getMessage());
+			}
 		}
 	}
 
