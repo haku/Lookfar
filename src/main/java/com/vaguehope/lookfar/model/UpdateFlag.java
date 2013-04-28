ddee3fe8a35b40e8e94bc05b8d992c4cd456c858
はく <haku@vaguehope.com>
2013-04-28 18:40:09 +0100

    Expose PENDING as update state.

--- src/main/java/com/vaguehope/lookfar/model/UpdateFlag.java
+++ src/main/java/com/vaguehope/lookfar/model/UpdateFlag.java
@@ -5,15 +5,16 @@ import com.vaguehope.lookfar.threshold.ThresholdStatus;
 
 public enum UpdateFlag {
 
+	PENDING,
 	OK,
 	EXPIRED,
 	INVALID,
 	WARNING, ;
 
 	public static UpdateFlag fromThresholdAndExpire (final ThresholdStatus t, final ExpireStatus e) {
-		if ((t == ThresholdStatus.UNDEFINED || t == ThresholdStatus.OK)
-				&& (e == ExpireStatus.OK)) return OK;
+		if ((t == ThresholdStatus.UNDEFINED || t == ThresholdStatus.OK) && e == ExpireStatus.OK) return OK;
 		if (e == ExpireStatus.EXPIRED) return EXPIRED;
+		if (e == ExpireStatus.PENDING) return PENDING;
 		if (t == ThresholdStatus.EXCEEDED) return WARNING;
 		if (t == ThresholdStatus.INVALID) return INVALID;
 		return WARNING;
