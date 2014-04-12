eba604d772671631e35433f821ce0f4ecb9bda1f
はく <haku@vaguehope.com>
2014-04-12 17:22:58 +0100

    Print divider row between nodes on /text.

--- src/main/java/com/vaguehope/lookfar/model/UpdateHelper.java
+++ src/main/java/com/vaguehope/lookfar/model/UpdateHelper.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.common.base.Objects;
 import com.google.common.collect.Table;
 import com.google.common.collect.TreeBasedTable;
 import com.vaguehope.lookfar.util.AsciiTable;
@@ -19,17 +20,29 @@ public final class UpdateHelper {
 	}
 
 	public static void printUpdates (final Collection<Update> updates, final HttpServletResponse resp) throws IOException {
-		Table<Integer, String, String> table = TreeBasedTable.create();
+		final Table<Integer, String, String> table = TreeBasedTable.create();
 		int i = 0;
-		for (Update u : updates) {
+		String lastNode = null;
+		boolean firstOfNode = false;
+		for (final Update u : updates) {
 			Integer row = Integer.valueOf(i++);
-			table.put(row, "node", u.getNode());
+
+			if (!Objects.equal(u.getNode(), lastNode)) {
+				table.put(row, "node", AsciiTable.ROW_DIVIDER);
+				row = Integer.valueOf(i++);
+				lastNode = u.getNode();
+				firstOfNode = true;
+			}
+
+			if (firstOfNode) table.put(row, "node", u.getNode());
 			table.put(row, "updated", u.getUpdated() == null ? NULL : DateFormatFactory.format(u.getUpdated()));
 			table.put(row, "key", String.valueOf(u.getKey()));
 			table.put(row, "value", String.valueOf(u.getValue()));
 			table.put(row, "threshold", u.getThreshold() == null ? NULL : u.getThreshold());
 			table.put(row, "expire", u.getExpire() == null ? NULL : u.getExpire());
 			table.put(row, "flag", u.calculateFlag().toString());
+
+			firstOfNode = false;
 		}
 		AsciiTable.printTable(table, new String[] { "node", "updated", "key", "value", "threshold", "expire", "flag" }, resp);
 	}
--- src/main/java/com/vaguehope/lookfar/util/AsciiTable.java
+++ src/main/java/com/vaguehope/lookfar/util/AsciiTable.java
@@ -7,82 +7,100 @@ import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.common.base.Objects;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Table;
 
 public final class AsciiTable {
 
+	public static final String ROW_DIVIDER = "*--";
+
 	private static final String COLUMN_SEPARATOR = " | ";
+	private static final String COLUMN_SEPARATOR_DIV_ROW = "-|-";
 
 	private AsciiTable () {
 		throw new AssertionError();
 	}
 
-	public static <R, C, V> void printTable (Table<R, C, V> table, C[] columnOrder, HttpServletResponse resp) throws IOException {
+	public static <R, C, V> void printTable (final Table<R, C, V> table, final C[] columnOrder, final HttpServletResponse resp) throws IOException {
 		resp.setContentType(Http.CONTENT_TYPE_HTML);
-		PrintWriter w = resp.getWriter();
+		final PrintWriter w = resp.getWriter();
 		w.println("<!DOCTYPE html><html><body><pre>");
 		printTable(table, columnOrder, w);
 		w.println("</pre></body></html>");
 	}
 
-	public static <R, C, V> void printTable (Table<R, C, V> table, C[] columnOrder, PrintWriter w) {
-		int rowKeyWidth = findRowKeyWidest(table);
-		Map<C, Integer> colWidths = measureColumnWidths(table, columnOrder);
+	public static <R, C, V> void printTable (final Table<R, C, V> table, final C[] columnOrder, final PrintWriter w) {
+		final int rowKeyWidth = findRowKeyWidest(table);
+		final Map<C, Integer> colWidths = measureColumnWidths(table, columnOrder);
 		printHeader(columnOrder, rowKeyWidth, colWidths, w);
 
 		printRows(table, columnOrder, rowKeyWidth, colWidths, w);
 	}
 
-	private static <R, C, V> int findRowKeyWidest (Table<R, C, V> table) {
+	private static <R, C, V> int findRowKeyWidest (final Table<R, C, V> table) {
 		int rowKeyWidth = 1;
-		for (R rowKey : table.rowKeySet()) {
+		for (final R rowKey : table.rowKeySet()) {
 			rowKeyWidth = Math.max(rowKeyWidth, rowKey.toString().length());
 		}
 		return rowKeyWidth;
 	}
 
-	private static <R, C, V> Map<C, Integer> measureColumnWidths (Table<R, C, V> table, C[] columnNames) {
-		Map<C, Integer> colWidths = Maps.newHashMap();
+	private static <R, C, V> Map<C, Integer> measureColumnWidths (final Table<R, C, V> table, final C[] columnNames) {
+		final Map<C, Integer> colWidths = Maps.newHashMap();
 		if (table.size() > 0) {
-			for (Entry<C, Map<R, V>> column : table.columnMap().entrySet()) {
+			for (final Entry<C, Map<R, V>> column : table.columnMap().entrySet()) {
 				int width = column.getKey().toString().length();
-				for (V cell : column.getValue().values()) {
+				for (final V cell : column.getValue().values()) {
 					width = Math.max(width, cell.toString().length());
 				}
 				colWidths.put(column.getKey(), Integer.valueOf(width));
 			}
 		}
 		else {
-			for (C col : columnNames) {
+			for (final C col : columnNames) {
 				colWidths.put(col, Integer.valueOf(col.toString().length()));
 			}
 		}
 		return colWidths;
 	}
 
-	private static <C> void printHeader (C[] columnOrder, int rowKeyWidth, Map<C, Integer> colWidths, PrintWriter w) {
+	private static <C> void printHeader (final C[] columnOrder, final int rowKeyWidth, final Map<C, Integer> colWidths, final PrintWriter w) {
 		w.print(padRight("", rowKeyWidth));
-		for (C col : columnOrder) {
+		for (final C col : columnOrder) {
 			w.print(COLUMN_SEPARATOR);
 			w.print(padRight(col, colWidths.get(col).intValue()));
 		}
 		w.println();
 	}
 
-	private static <R, C, V> void printRows (Table<R, C, V> table, C[] columnOrder, int rowKeyWidth, Map<C, Integer> colWidths, PrintWriter w) {
-		for (Entry<R, Map<C, V>> entry : table.rowMap().entrySet()) {
+	private static <R, C, V> void printRows (final Table<R, C, V> table, final C[] columnOrder, final int rowKeyWidth, final Map<C, Integer> colWidths, final PrintWriter w) {
+		for (final Entry<R, Map<C, V>> entry : table.rowMap().entrySet()) {
+			final boolean divRow = Objects.equal(ROW_DIVIDER, entry.getValue().get(columnOrder[0]));
+
 			w.print(padRight(entry.getKey(), rowKeyWidth));
-			for (C col : columnOrder) {
-				w.print(COLUMN_SEPARATOR);
-				w.print(padRight(entry.getValue().get(col), colWidths.get(col).intValue()));
+			for (final C col : columnOrder) {
+				if (divRow) {
+					w.print(COLUMN_SEPARATOR_DIV_ROW);
+					printRepeatChar(w, colWidths.get(col).intValue(), '-');
+				}
+				else {
+					w.print(COLUMN_SEPARATOR);
+					w.print(padRight(entry.getValue().get(col), colWidths.get(col).intValue()));
+				}
 			}
 			w.println();
 		}
 	}
 
-	private static String padRight (Object s, int n) {
-		return String.format("%1$-" + n + "s", s == null ? "null" : s.toString());
+	private static void printRepeatChar (final PrintWriter w, final int count, final char c) {
+		for (int i = 0; i < count; i ++) {
+			w.print(c);
+		}
+	}
+
+	private static String padRight (final Object s, final int n) {
+		return String.format("%1$-" + n + "s", StringHelper.toStringOrDefault(s, ""));
 	}
 
 }
