45de2914fc42db0a26818a8d11fe389d661f9d50
haku <git@haku.me>
2014-05-26 12:12:44 +0100

    Fetch, format and show updates.

---
+++ client.android/src/main/java/com/vaguehope/lookfar/android/model/UpdateAdapter.java
@@ -0,0 +1,90 @@
+package com.vaguehope.lookfar.android.model;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.vaguehope.lookfar.android.R;
+
+public class UpdateAdapter extends BaseAdapter {
+
+	private final List<Update> updates;
+	private final LayoutInflater layoutInflater;
+
+	public UpdateAdapter (final Context context, final List<Update> updates) {
+		this.updates = updates;
+		this.layoutInflater = LayoutInflater.from(context);
+	}
+
+	public Update getUpdate (final int position) {
+		if (this.updates == null) return null;
+		if (position >= this.updates.size()) return null;
+		return this.updates.get(position);
+	}
+
+	@Override
+	public int getCount () {
+		return this.updates == null ? 0 : this.updates.size();
+	}
+
+	@Override
+	public Object getItem (final int position) {
+		return getUpdate(position);
+	}
+
+	@Override
+	public long getItemId (final int position) {
+		return position;
+	}
+
+	@Override
+	public View getView (final int position, final View convertView, final ViewGroup parent) {
+		View view = convertView;
+		RowView rowView;
+		if (view == null) {
+			view = this.layoutInflater.inflate(R.layout.updatelistrow, null);
+			rowView = new RowView(view);
+			view.setTag(rowView);
+		}
+		else {
+			rowView = (RowView) view.getTag();
+		}
+		rowView.setItem(this.updates.get(position));
+		return view;
+	}
+
+	private static class RowView {
+
+		private final TextView node;
+		private final TextView flag;
+		private final TextView key;
+		private final TextView value;
+
+		public RowView (final View view) {
+			this((TextView) view.findViewById(R.id.txtNode),
+					(TextView) view.findViewById(R.id.txtFlag),
+					(TextView) view.findViewById(R.id.txtKey),
+					(TextView) view.findViewById(R.id.txtValue));
+		}
+
+		public RowView (final TextView node, final TextView flag, final TextView key, final TextView value) {
+			this.node = node;
+			this.flag = flag;
+			this.key = key;
+			this.value = value;
+		}
+
+		public void setItem (final Update item) {
+			this.node.setText(item.getNode());
+			this.flag.setText(item.getFlag());
+			this.key.setText(item.getKey());
+			this.value.setText(item.getValue());
+		}
+	}
+
+}
---
+++ client.android/src/main/java/com/vaguehope/lookfar/android/util/StringHelper.java
@@ -0,0 +1,42 @@
+package com.vaguehope.lookfar.android.util;
+
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+public final class StringHelper {
+
+	public static final Pattern URL_PATTERN = Pattern.compile("\\(?\\b(https?://|www[.])[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]");
+
+	private static final String ELIPSE = "...";
+
+	private StringHelper () {
+		throw new AssertionError();
+	}
+
+	public static boolean isEmpty (final String s) {
+		return s == null || s.isEmpty();
+	}
+
+	public static String maxLength(final String s, final int len) {
+		if (s.length() < len) return s;
+		return s.substring(0, len - ELIPSE.length()) + ELIPSE;
+	}
+
+	public static String maxLengthEnd(final String s, final int len) {
+		if (s.length() <= len) return s;
+		return ELIPSE + s.substring(s.length() - (len - ELIPSE.length()));
+	}
+
+	public static boolean safeContainsIgnoreCase (final String s, final String lookFor) {
+		if (s == null) return lookFor == null;
+		if (lookFor == null) return false;
+		return s.toLowerCase(Locale.UK).contains(lookFor.toLowerCase(Locale.UK));
+	}
+
+	public static String firstLine(final String s) {
+		if (s == null) return s;
+		final int x = s.indexOf('\n');
+		return x >= 0 ? s.substring(0, x) : s;
+	}
+
+}
